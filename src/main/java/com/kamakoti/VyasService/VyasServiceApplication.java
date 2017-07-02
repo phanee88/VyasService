32d3283aeaaccc613052277c94493a7a4a2ef3b5
unknown <phanee.c@gmail.com>
2017-07-02 17:26:34 +0530

    Initial Checkout

---
+++ src/main/java/com/kamakoti/VyasService/VyasServiceApplication.java
@@ -0,0 +1,12 @@
+package com.kamakoti.VyasService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VyasServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VyasServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kamakoti/VyasService/VyasServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kamakoti.VyasService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VyasServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
